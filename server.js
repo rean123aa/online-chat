be3e622b1c283dd85c1aa14a7f95f6c4b5ae9291
rean123aa <jessica194588@gmail.com>
2025-10-20 19:48:52 -0400

    Updated chat layout / feature XYZ

--- server.js
+++ server.js
@@ -3,71 +3,69 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
-let messages = []; // Store all chat messages
+// Messages and users storage (in-memory)
+let messages = [];
 let users = [];
 
-io.on('connection', socket => {
-  let username = '';
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
 
-  // Set username
-  socket.on('set username', name => {
-    username = name;
-    users.push(username);
-    io.emit('update users', users);
-    socket.emit('load messages', messages);
-  });
+    // Set username
+    socket.on('set username', username => {
+        socket.username = username;
+        users.push(username);
+        io.emit('update users', users);
+        socket.emit('load messages', messages);
+    });
 
-  // New message
-  socket.on('chat message', text => {
-    const msg = {
-      id: Date.now().toString(), // unique ID
-      user: username,
-      text,
-      time: new Date().toLocaleTimeString(),
-      reactions: {} // for live emoji reactions
-    };
-    messages.push(msg);
-    io.emit('chat message', msg);
-  });
+    // Chat message
+    socket.on('chat message', text => {
+        const msg = {
+            id: Date.now().toString(),
+            user: socket.username,
+            text,
+            time: new Date().toLocaleTimeString(),
+            reactions: {}
+        };
+        messages.push(msg);
+        io.emit('chat message', msg);
+    });
 
-  // Edit message
-  socket.on('edit message', ({ id, newText }) => {
-    const msg = messages.find(m => m.id === id);
-    if(msg && msg.user === username) { // only allow sender to edit
-      msg.text = newText;
-      io.emit('edit message', { id, newText });
-    }
-  });
+    // Edit message
+    socket.on('edit message', ({id, newText}) => {
+        const msg = messages.find(m => m.id === id);
+        if(msg && msg.user === socket.username){
+            msg.text = newText;
+            io.emit('edit message', {id, newText});
+        }
+    });
 
-  // Delete message
-  socket.on('delete message', id => {
-    const msg = messages.find(m => m.id === id);
-    if(msg && msg.user === username) { // only allow sender to delete
-      messages = messages.filter(m => m.id !== id);
-      io.emit('delete message', id);
-    }
-  });
+    // Delete message
+    socket.on('delete message', id => {
+        const msgIndex = messages.findIndex(m => m.id === id);
+        if(msgIndex !== -1 && messages[msgIndex].user === socket.username){
+            messages.splice(msgIndex,1);
+            io.emit('delete message', id);
+        }
+    });
 
-  // Live emoji reactions
-  socket.on('add reaction', ({ messageId, emoji }) => {
-    const msg = messages.find(m => m.id === messageId);
-    if(msg) {
-      msg.reactions[emoji] = (msg.reactions[emoji] || 0) + 1;
-      io.emit('update reactions', { messageId, reactions: msg.reactions });
-    }
-  });
+    // Reactions
+    socket.on('add reaction', ({ messageId, emoji }) => {
+        const msg = messages.find(m => m.id === messageId);
+        if(!msg.reactions[emoji]) msg.reactions[emoji] = 0;
+        msg.reactions[emoji]++;
+        io.emit('update reactions', { messageId, reactions: msg.reactions });
+    });
 
-  // Disconnect
-  socket.on('disconnect', () => {
-    users = users.filter(u => u !== username);
-    io.emit('update users', users);
-  });
+    socket.on('disconnect', () => {
+        users = users.filter(u => u !== socket.username);
+        io.emit('update users', users);
+        console.log('A user disconnected:', socket.id);
+    });
 });
 
-// Start server
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
